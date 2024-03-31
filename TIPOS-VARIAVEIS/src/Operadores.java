63a95dbdb71c5e34bdac5b74021f38398aa5ab18
Reginaldo <reginaldoengenheiro16@gmail.com>
2024-03-31 17:28:46 -0300

    Operadores

---
+++ TIPOS-VARIAVEIS/src/Operadores.java
@@ -0,0 +1,23 @@
+public class Operadores {
+    public static void main(String[] args) {
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        System.out.println("O númeroUm é igual a númrodois?" + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("O númeroUm é diferente númerodois?" + simNao);
+
+        simNao = numero1 < numero2;
+
+        System.out.println("O númeroUm é maior númerodois?" + simNao);
+
+
+
+
+
+    }
+}
--- TIPOS-VARIAVEIS/src/TiposVariaveis.java
+++ TIPOS-VARIAVEIS/src/TiposVariaveis.java
@@ -1,5 +1,16 @@
-public class App {
+public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+      double salarioMinimo = 2500;
+
+      short numeroCurto = 1;
+      int numeroNormal = numeroCurto;
+      short numeroCurto2 = (short) numeroNormal;
+
+      int numero = 1;
+      System.out.println(numero);
+      final double VALOR DE PI  = 3.14;
+
+
+      
     }
 }
